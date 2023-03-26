cebdc98e444704febb5b1c02b5fe029e8f8a292c
Vb1221 <106549600+Vb1221@users.noreply.github.com>
2023-03-26 18:56:52 +0300

    HW1

---
+++ HW1/script.js
@@ -0,0 +1,25 @@
+alert('Перше завдання')
+
+let firstNumber = prompt("Enter First Number");
+let secondNumber = prompt("Enter Second Number");
+let thirdNumber = prompt("Enter third Number");
+
+console.log((+firstNumber + +secondNumber + +thirdNumber)/3);
+
+alert('Друге завдання')
+
+let firstValue = prompt("Enter First Number");
+let secondValue = prompt("Enter Second Number");
+
+let equal = firstValue == secondValue;
+console.log(equal)
+
+
+
+alert("Третє Завдання")
+
+ let price = 12999;
+ let discount = prompt('Enter your discount');
+ console.log(discount);
+ let discountPrice = price - ((price/100) * +discount);
+ console.log(discountPrice);
