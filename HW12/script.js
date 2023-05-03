69088ec456e4ece6e50c30913ea78d2fc765978f
Vb1221 <106549600+Vb1221@users.noreply.github.com>
2023-05-03 17:09:18 +0300

    hw12

--- HW12/script.js
+++ HW12/script.js
@@ -1,3 +1,33 @@
 window.onload = () => {
+  let strInput = document.querySelector('.inputUpperLetter')
+ 
+  function countUppercaseLetters(string) {
+    let regex = /[A-Z]/g; 
+    let  matches = string.match(regex); 
+    return matches ? matches.length : 0; 
+  }
+  
+  strInput.onclick = () => {
+    let string = strInput.value
+    let count = countUppercaseLetters(string); 
+    console.log(count); 
+  }
+  
+
+
+  let numInput = document.querySelector('.returnNumber')
+
+  function returnNum(result){
+    let patternNum = /\D/g 
+    let replaceL = result.replace(patternNum, '')
+    return replaceL
+  }
+
+  numInput.onclick = () =>{
+    let result = numInput.value 
+    let numResult = returnNum(result)
+    console.log(numResult)
+
+  }
 
 }
